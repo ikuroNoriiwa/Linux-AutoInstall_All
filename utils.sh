9319e7b7733d6fa621c6a5c00876c01895a1c281
vagrant <amaelcattaneo@gmail.com>
2021-12-13 12:45:57 +0100

    change errorr :

--- utils.sh
+++ utils.sh
@@ -83,9 +83,9 @@ ssh_key_creation(){
     #                           #
     #############################
 
-    for username in $(grep -E "((bash)|(sh)):" /etc/passwd|tail -1): do
+    for username in $(grep -E "((bash)|(sh)):" /etc/passwd|tail -1); do
 
-        if [[ "$username" == "root" ]]: then
+        if [[ "$username" == "root" ]]; then
 
         mkdir -v /home/$username/.ssh
         chmod -v 700 /home/$user/.ssh
