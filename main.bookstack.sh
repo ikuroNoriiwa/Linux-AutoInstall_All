1e82f6b6a680f1408fb9120e88373f49f49ed491
YoNixNeXRo <nathan.75015@yahoo.com>
2021-12-15 08:58:29 +0100

    true main boockstack

--- main.bookstack.sh
+++ main.bookstack.sh
@@ -2,8 +2,12 @@
 
 main_bookstack(){
 
-    updatedb
-    # reboot
+    ssl_keys
+    requirements_bookstack_mariadb_php
+    conf_mariadb
+    conf_nginx
+    bookstack_en
+    conf_bookstack_for_SSO
 }
 
 main_bookstack
